b8d7e56509fa010bb2ad548fc5302bcabc1a99bd
pcabellologic <pinochobionico@gmail.com>
2024-04-24 20:40:48 +0200

    Añadido carrusel del inicio, creado el estilo (incluye los cuatro nuevos archivos de css), paleta de colores ya conformada, plantilla de HTML por terminar

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ api.use(express.static(__dirname + '/public'));
 api.listen(3000, ()=> {
     console.log('Server up and running')
 });
+
+// ### RUTAS ###
+
+// app.get('/', function(req, res){
+//     if (err) throw err;
+//       res.render('index', {
+//         title: 'modelos',
+//         modelos: results
+//     });
+// });
