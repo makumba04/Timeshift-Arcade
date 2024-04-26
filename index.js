4d00f23612bed4c604d8f91168a1a7493b11f200
pcabellologic <pinochobionico@gmail.com>
2024-04-26 11:58:38 +0200

    Añadido al index.js la plantilla para rutas de otras subpáginas, queda generar el sistema de la bd

--- index.js
+++ index.js
@@ -15,3 +15,16 @@ app.listen(3000, ()=> {
 app.get('/', function(req, res){
     res.render('index');
 });
+
+// Hay que crear la BD y las tablas antes de correr esto
+
+// app.get('/games/:id', function(req, res){
+//     const {id} = req.params;
+//     // db.query('SELECT modelos.nombreModelo, modelos.intro, modelos.historia, modelos.legado, generaciones.id, generaciones.rutaPortada, generaciones.nombreGeneracion, generaciones.periodo FROM modelos INNER JOIN generaciones ON modelos.id = generaciones.modelo_id WHERE modelos.id = ?', [id], (err, results) => {
+//     //   if (err) throw err;
+//     //   res.render('ModelTemplate', {
+//     //     title: 'modelo',
+//     //     modelo: results
+//     //   });
+//     // })
+// });
