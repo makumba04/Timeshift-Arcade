0afc71adf4325eee3607e2a8c65de30cf24ea776
pcabellologic <pinochobionico@gmail.com>
2024-05-10 16:37:20 +0200

    Terminados CREATE y DELETE del CRUD principal. Funcionan ambos en las 3 tablas dentro de la DB. Queda añadir UPDATE para esas 3, confirmar que funcionen y añadir las funciones de editar al perfil de usuario para la foto de perfil y la biografía del usuario (Queda por aún el tema subida de imagenes para el pfp).

--- index.js
+++ index.js
@@ -62,40 +62,274 @@ app.get('/', function(req, res){
     })
 });
 
-function requireAdmin(req, res, next) {
-    const user_role = req.session.userRoleLevel;
+// -- ADMIN PANEL ROUTES
 
-    if(user_role != 3) {
-        res.status(403).send("You don't have the authorization to enter this page");
-    } else {
-        next();
+    function requireAdmin(req, res, next) {
+        const user_role = req.session.userRoleLevel;
+
+        if(user_role != 3) {
+            res.status(403).send("You don't have the authorization to enter this page");
+        } else {
+            next();
+        }
     }
-}
 
-app.get('/admin-panel', requireAdmin, function(req, res){
-    res.render('admin-panel')
-});
-
-app.get('/admin_panel_categories', (req, res) => {
-    db.query('SELECT * FROM category', (err, results) =>{
-        if(err) throw err;
-        res.json(results)
+    app.get('/admin-panel', requireAdmin, function(req, res){
+        res.render('admin-panel')
     });
-});
 
-app.get('/admin_panel_games', (req, res) => {
-    db.query('SELECT game.*, category.* FROM game INNER JOIN category ON game.category_type = category.category_id', (err, results) =>{
-        if(err) throw err;
-        res.json(results)
+    app.get('/admin_panel_categories', (req, res) => {
+        db.query('SELECT * FROM category', (err, results) =>{
+            if(err) throw err;
+            res.json(results)
+        });
     });
-});
 
-app.get('/admin_panel_users', (req, res) => {
-    db.query('SELECT * FROM users', (err, results) =>{
-        if(err) throw err;
-        res.json(results)
+    app.get('/admin_panel_games', (req, res) => {
+        db.query('SELECT game.*, category.* FROM game INNER JOIN category ON game.category_type = category.category_id', (err, results) =>{
+            if(err) throw err;
+            res.json(results)
+        });
     });
-});
+
+    app.get('/admin_panel_users', (req, res) => {
+        db.query('SELECT * FROM users', (err, results) =>{
+            if(err) throw err;
+            res.json(results)
+        });
+    });
+    
+    // -- CREATE GAME
+
+        app.get("/admin_panel_games/create", async (req, res) => {
+            try {
+                res.render("create-game-form");
+            } catch (error) {
+                console.error("Error displaying confirmation page:", error);
+                res.status(500).send("An error occurred while displaying confirmation page");
+            }
+        });
+
+        app.post("/admin_panel_users/create_action", async (req, res) => {
+            try {
+                // Get the request body
+                const { username, email, password } = req.body;
+
+                // Validate input
+                if (!username || !email || !password) {
+                    return res.status(400).send("Missing required fields");
+                }
+
+                // Hash the password
+                const hashedPassword = await bcrypt.hash(password, 10);
+
+                // Insert the user into the database
+                const queryResult = await query(
+                    `INSERT INTO users (username, email, passwd, user_role) VALUES (?, ?, ?, 1)`,
+                    [username, email, hashedPassword]
+                );
+
+                res.redirect('/admin-panel');
+            } catch (error) {
+                // Handle errors
+                console.error("Error registering user:", error);
+                res.status(500).send("An error occurred while registering the user");
+            }
+        })
+
+    // -- CREATE CATEGORY
+
+        app.get("/admin_panel_category/create", async (req, res) => {
+            try {
+                res.render("create-category-form");
+            } catch (error) {
+                console.error("Error displaying form page:", error);
+                res.status(500).send("An error occurred while displaying form page");
+            }
+        });
+
+        app.post("/admin_panel_category/create_action", async (req, res) => {
+            try {
+                // Get the request body
+                const { category_cover, category_name, category_description } = req.body;
+
+                // Validate input
+                if (!category_cover || !category_name || !category_description) {
+                    return res.status(400).send("Missing required fields");
+                }
+
+                // Insert the user into the database
+                const queryResult = await query(
+                    `INSERT INTO category (category_cover, category_name, category_description) VALUES (?, ?, ?)`,
+                    [category_cover, category_name, category_description]
+                );
+
+                res.redirect('/admin-panel');
+            } catch (error) {
+                // Handle errors
+                console.error("Error registering category:", error);
+                res.status(500).send("An error occurred while registering the category");
+            }
+        })
+    
+    // -- DELETE CATEGORY
+        app.get("/admin_panel_category/delete/confirm/:categoryId", async (req, res) => {
+            try {
+                const { categoryId } = req.params;
+                const categoryData = await query("SELECT * FROM category WHERE category_id = ?", [categoryId]);
+        
+                res.render("delete-category-confirm", { category_data: categoryData[0] });
+            } catch (error) {
+                console.error("Error displaying confirmation page:", error);
+                res.status(500).send("An error occurred while displaying confirmation page");
+            }
+        });
+
+        app.post("/admin_panel_category/delete/:categoryId", async (req, res) => {
+            try {
+                const { categoryId } = req.params;
+
+                await query("DELETE FROM category WHERE category_id = ?", [categoryId]);
+                // res.status(200).send("User deleted successfully");
+                
+                res.redirect('/admin-panel');
+            } catch (error) {
+
+                console.error("Error deleting user:", error);
+                res.status(500).send("An error occurred while deleting the category");
+            }
+        });
+
+    // -- CREATE GAME
+
+    app.get("/admin_panel_games/create", async (req, res) => {
+        try {
+            res.render("create-game-form");
+        } catch (error) {
+            console.error("Error displaying form page:", error);
+            res.status(500).send("An error occurred while displaying form page");
+        }
+    });
+
+    app.post("/admin_panel_games/create_action", async (req, res) => {
+        try {
+            // Get the request body
+            const { category_type, game_cover, game_name, game_description, localpath, htp, featured } = req.body;
+
+            // Validate input
+            if (!category_type || !game_cover || !game_name || !game_description || !localpath || !htp || !featured) {
+                return res.status(400).send("Missing required fields");
+            }
+
+            // Insert the user into the database
+            const queryResult = await query(
+                `INSERT INTO game (category_type, game_cover, game_name, game_description, localpath, htp, featured ) VALUES (?, ?, ?, ?, ?, ?, ?)`,
+                [category_type, game_cover, game_name, game_description, localpath, htp, featured]
+            );
+
+            res.redirect('/admin-panel');
+        } catch (error) {
+            // Handle errors
+            console.error("Error registering game:", error);
+            res.status(500).send("An error occurred while registering the game");
+        }
+    })
+    
+    // -- DELETE GAME
+        app.get("/admin_panel_games/delete/confirm/:gameId", async (req, res) => {
+            try {
+                const { gameId } = req.params;
+                const gameData = await query("SELECT * FROM game WHERE game_id = ?", [gameId]);
+        
+                res.render("delete-game-confirm", { game_data: gameData[0] });
+            } catch (error) {
+                console.error("Error displaying confirmation page:", error);
+                res.status(500).send("An error occurred while displaying confirmation page");
+            }
+        });
+
+        app.post("/admin_panel_games/delete/:gameId", async (req, res) => {
+            try {
+                const { gameId } = req.params;
+
+                await query("DELETE FROM game WHERE game_id = ?", [gameId]);
+                // res.status(200).send("User deleted successfully");
+                
+                res.redirect('/admin-panel');
+            } catch (error) {
+
+                console.error("Error deleting user:", error);
+                res.status(500).send("An error occurred while deleting the game");
+            }
+        });
+
+    // -- CREATE USER
+
+        app.get("/admin_panel_users/create", async (req, res) => {
+            try {
+                res.render("create-user-form");
+            } catch (error) {
+                console.error("Error displaying form page:", error);
+                res.status(500).send("An error occurred while displaying form page");
+            }
+        });
+
+        app.post("/admin_panel_users/create_action", async (req, res) => {
+            try {
+                // Get the request body
+                const { username, email, password } = req.body;
+        
+                // Validate input
+                if (!username || !email || !password) {
+                    return res.status(400).send("Missing required fields");
+                }
+        
+                // Hash the password
+                const hashedPassword = await bcrypt.hash(password, 10);
+        
+                // Insert the user into the database
+                const queryResult = await query(
+                    `INSERT INTO users (username, email, passwd, user_role) VALUES (?, ?, ?, 1)`,
+                    [username, email, hashedPassword]
+                );
+        
+                res.redirect('/admin-panel');
+            } catch (error) {
+                // Handle errors
+                console.error("Error registering user:", error);
+                res.status(500).send("An error occurred while registering the user");
+            }
+        })
+
+    // -- DELETE USER
+        app.get("/admin_panel_users/delete/confirm/:userId", async (req, res) => {
+            try {
+                const { userId } = req.params;
+                const userData = await query("SELECT * FROM users WHERE user_id = ?", [userId]);
+        
+                res.render("delete-user-confirm", { user_data: userData[0] });
+            } catch (error) {
+                console.error("Error displaying confirmation page:", error);
+                res.status(500).send("An error occurred while displaying confirmation page");
+            }
+        });
+
+        app.post("/admin_panel_users/delete/:userId", async (req, res) => {
+            try {
+                const { userId } = req.params;
+
+                await query("DELETE FROM users WHERE user_id = ?", [userId]);
+                // res.status(200).send("User deleted successfully");
+                
+                res.redirect('/admin-panel')
+            } catch (error) {
+
+                console.error("Error deleting user:", error);
+                res.status(500).send("An error occurred while deleting the user");
+            }
+        });
+
+// -- ADMIN PANEL ROUTES
 
 app.get('/my_profile/:userId', function(req, res){
     const {userId} = req.params;
@@ -107,6 +341,14 @@ app.get('/my_profile/:userId', function(req, res){
         });
     })
 });
+
+app.get('/user_linked_games/:userid', function(req, res){
+    const {userId} = req.params;
+    db.query('SELECT game.* FROM game INNER JOIN saves ON game.game_id = saves.game_id INNER JOIN users ON users.user_id = saves.user_id WHERE users.user_id = ?', [userId], (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    })
+})
 
 app.get('/categories', function(req, res){
     db.query('SELECT * FROM category', (err, results) => {
--- public/js/functions.js
+++ public/js/functions.js
@@ -26,6 +26,15 @@ function searchAllUsers() {
     .catch(error => console.error('Error:', error));
 }
 
+function searchUserGames(user_id) {
+    fetch(`http://localhost:3000/user_linked_games/${user_id}`)
+    .then(response => response.json())
+    .then(data => {
+        showUserGames(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
+
 // Data-showing functions: Receive all data from the Data-fetching functions and elaborate tables to show the resulting data
 
 function showAllCategories(categories) {
@@ -41,6 +50,7 @@ function showAllCategories(categories) {
     var thead = document.createElement('thead');
     var tr_thead = document.createElement('tr');
     var tbody = document.createElement('tbody');
+    var a_create_btn = document.createElement('a');
 
     headers.forEach(header => {
         var th = document.createElement('th');
@@ -55,12 +65,19 @@ function showAllCategories(categories) {
         
         var tr_body = document.createElement('tr');
 
-        tr_body.innerHTML = `<td>${category.category_id}</td><td>${category.category_name}</td><td>${category.category_description}</td><td class="admin-panel-actions"><button onclick="editCategory()">Edit</button><button onclick="deleteCategory()">Delete</button></td>`;
+        tr_body.innerHTML = `<td>${category.category_id}</td><td>${category.category_name}</td><td>${category.category_description}</td><td class="admin-panel-actions"><button id="edit-btn">Edit</button><a href='/admin_panel_category/delete/confirm/${category.category_id}'><button id="delete-btn">Delete</button></a></td>`;
         tbody.appendChild(tr_body);
     })
 
     table.appendChild(tbody);
+    table.style.marginBottom = '45px';
+    
+    a_create_btn.innerHTML = `<button class="create-btn">Add new category</button>`;
+    a_create_btn.href = `/admin_panel_category/create`;
+    a_create_btn.classList.add('create-btn');
+
     container.appendChild(table);
+    container.appendChild(a_create_btn)
 }
 
 function showAllGames(games) {
@@ -76,6 +93,7 @@ function showAllGames(games) {
     var thead = document.createElement('thead');
     var tr_thead = document.createElement('tr');
     var tbody = document.createElement('tbody');
+    var a_create_btn = document.createElement('a');
 
     headers.forEach(header => {
         var th = document.createElement('th');
@@ -90,12 +108,19 @@ function showAllGames(games) {
         
         var tr_body = document.createElement('tr');
 
-        tr_body.innerHTML = `<td>${game.game_id}</td><td>${game.category_name}</td><td>${game.game_name}</td><td class="admin-panel-actions"><button onclick="editCategory()">Edit</button><button onclick="deleteCategory()">Delete</button></td>`;
+        tr_body.innerHTML = `<td>${game.game_id}</td><td>${game.category_name}</td><td>${game.game_name}</td><td class="admin-panel-actions"><button id="edit-btn">Edit</button><a href='/admin_panel_games/delete/confirm/${game.game_id}'><button id="delete-btn">Delete</button></a></td>`;
         tbody.appendChild(tr_body);
     })
 
     table.appendChild(tbody);
+    table.style.marginBottom = '45px';
+
+    a_create_btn.innerHTML = `<button class="create-btn">Add new game</button>`;
+    a_create_btn.href = `/admin_panel_games/create`;
+    a_create_btn.classList.add('create-btn');
+
     container.appendChild(table);
+    container.appendChild(a_create_btn);
 }
 
 function showAllUsers(users) {
@@ -111,6 +136,7 @@ function showAllUsers(users) {
     var thead = document.createElement('thead');
     var tr_thead = document.createElement('tr');
     var tbody = document.createElement('tbody');
+    var a_create_btn = document.createElement('a');
 
     headers.forEach(header => {
         var th = document.createElement('th');
@@ -125,10 +151,76 @@ function showAllUsers(users) {
         
         var tr_body = document.createElement('tr');
 
-        tr_body.innerHTML = `<td>${user.user_id}</td><td>${user.username}</td><td>${user.user_role}</td><td class="admin-panel-actions"><button onclick="editCategory()">Edit</button><button onclick="deleteCategory()">Delete</button></td>`;
+        if(user.user_id == 1) {
+
+            tr_body.innerHTML = `<td>${user.user_id}</td><td>${user.username}</td><td>${user.user_role}</td><td></td>`;
+        } else {
+            tr_body.innerHTML = `<td>${user.user_id}</td><td>${user.username}</td><td>${user.user_role}</td><td class="admin-panel-actions"><a href='/admin_panel_users/edit/${user.user_id}'><button id="edit-btn">Edit</button></a></form><a href="/admin_panel_users/delete/confirm/${user.user_id}"><button id="delete-btn">Delete</button></a></form></td>`;
+        }
+
         tbody.appendChild(tr_body);
     })
 
     table.appendChild(tbody);
+    table.style.marginBottom = '45px';
+
+    a_create_btn.innerHTML = `<button class="create-btn">Add new user</button>`;
+    a_create_btn.href = `/admin_panel_users/create`;
+    a_create_btn.classList.add('create-btn');
+
     container.appendChild(table);
+    container.appendChild(a_create_btn);
+}
+
+function showUserGames(games) {
+
+    var linked_games = document.getElementById('profile-linked-games');
+
+    if (games.length > 0) {
+        
+        games.forEach(game => {
+
+            var section_game_block = document.createElement('section');
+            var div_game_desc = document.createElement('div');
+            var img_game_image = document.createElement('img');
+            var div_game_desc_txt = document.createElement('div');
+            var h3 = createElement('h3');
+            var p = createElement('p');
+            var a = createElement('a');
+            var button = createElement('button');
+
+            section_game_block.classList.add('game-block');
+            div_game_desc.classList.add('game-desc');
+            img_game_image.classList.add('game-image');
+            div_game_desc_txt.classList.add('game-desc-txt');
+
+            h3.text = `${game.game_name}`;
+            p.text = `${game.game_description}`;
+            a.href = `/games/${game.game_id}`;
+            button.text = 'Play';
+
+            a.append(button);
+
+            div_game_desc_txt.append(h3);
+            div_game_desc_txt.append(p);
+            div_game_desc_txt.append(a);
+
+            div_game_desc.append(img_game_image);
+            div_game_desc.append(div_game_desc_txt);
+
+            section_game_block.append(div_game_desc_txt);
+
+            linked_games.appendChild(section_game_block);
+        })
+    } else {
+
+        const p_no_games = document.createElement('p');
+        const text = "There are no linked games";
+        p_no_games.append(text);
+
+        linked_games.style.display = 'flex';
+        linked_games.style.justifyContent = 'center';
+        linked_games.style.alignItems = 'center';
+        linked_games.append(p_no_games);
+    }
 }
